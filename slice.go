7576207a347b0b534a50f2031a3b17e1a8409f76
bitfrost7 <15591017398@163.com>
2024-05-07 19:06:53 +0800

    add slicereduce

--- slice.go
+++ slice.go
@@ -1,10 +1,10 @@
 package goutils
 
-type joinable[K comparable] interface {
+type keyable[K comparable] interface {
 	key() K
 }
 
-func SliceJoin[K comparable, A joinable[K], B joinable[K]](a []A, b []B, join func(A, B) A) []A {
+func SliceJoin[K comparable, A keyable[K], B keyable[K]](a []A, b []B, join func(A, B) A) []A {
 	m := make(map[K]A)
 	for _, v := range a {
 		m[v.key()] = v
@@ -15,4 +15,21 @@ func SliceJoin[K comparable, A joinable[K], B joinable[K]](a []A, b []B, join fu
 		}
 	}
 	return a
+}
+
+func Slice2Map[K comparable, T keyable[K]](s []T) map[K]T {
+	m := make(map[K]T)
+	for _, v := range s {
+		m[v.key()] = v
+	}
+	return m
+}
+
+func SliceReduce[U any, V any](initial V, s1 []U, reduce func(V, U) V) []V {
+	result := make([]V, len(s1)+1)
+	result[0] = initial
+	for i, v := range s1 {
+		result[i+1] = reduce(result[i], v)
+	}
+	return result
 }
