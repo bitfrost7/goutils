3979b9b3e15d3a629a171cd705f645149437d6c7
bitfrost7 <15591017398@163.com>
2024-05-09 16:36:23 +0800

    Slice2MapWithConflict

--- slice.go
+++ slice.go
@@ -15,6 +15,15 @@ func SliceFilter[T any](s []T, filter func(v T) bool) []T {
 }
 
 // Slice2Map 将实现Mappable接口的slice转化为map
+//
+// 参数:
+//
+//	a: 	指定切片。
+//	key: 	返回切片类型中的索引。
+//
+// 返回值:
+//
+//	返回切片转化成map结构的结果。
 func Slice2Map[K comparable, T Mappable[K]](s []T) map[K]T {
 	m := make(map[K]T)
 	for _, v := range s {
@@ -24,6 +33,16 @@ func Slice2Map[K comparable, T Mappable[K]](s []T) map[K]T {
 }
 
 // Slice2MapWithConflict 将实现Mappable接口的slice转化为map 并指定冲突解决方式
+//
+// 参数:
+//
+//	  a: 	指定切片。
+//	  key: 	返回切片类型中的索引。
+//		 conflict:	当map中出现冲突时进行处理。
+//
+// 返回值:
+//
+//	返回切片转化成map结构的结果。
 func Slice2MapWithConflict[K comparable, T Mappable[K]](s []T, conflict func(old, new T) T) map[K]T {
 	m := make(map[K]T)
 	for _, v := range s {
